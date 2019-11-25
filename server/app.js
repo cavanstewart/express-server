3a64c40f6fe407f534207d7c35a2b29d68f3c8db
Cavan Stewart <cavanwstewart@gmail.com>
2019-11-25 15:38:51 -0800

    fixed now deployment

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 // import files and packages up here
-
+const express = require("express");
+const morgan = require("morgan");
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan("dev"));
+
+const router = express.Router();
+app.use("/", router);
+
+router.route("/").all(function(req, res, next) {
+  res.status(200).send("hello world");
+});
+
+const data = require("./data.json");
+
+router.route("/data").all(function(req, res, next) {
+  res.json(data);
+});
 
 // finally export the express application
 module.exports = app;
